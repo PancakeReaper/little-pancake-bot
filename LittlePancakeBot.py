f6c4fd6e8cb6e66017c16760c0e21bedfcd62abd
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2018-05-06 20:36:30 -0700

    Update LittlePancakeBot.py

--- LittlePancakeBot.py
+++ LittlePancakeBot.py
@@ -12,6 +12,11 @@ from urllib import request as uRequest
 translate = {"ロイヤル":"Sword", "ウィッチ":"Rune", "ネメシス":"Portal", "ビショップ":"Haven", 
 			 "ヴァンパイア":"Blood", "ドラゴン":"Dragon", "エルフ":"Forest", "ネクロマンサー":"Shadow"}
 
+eightBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", 
+	    "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", 
+	     "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", 
+	     "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
 client = discord.Client()
 
 @client.event
@@ -170,6 +175,10 @@ async def on_message(message):
 
 	elif message.content.startswith("Who's the best shadowverse player"):
 		await client.send_message(message.channel, "Why " + os.environ.get("BEST_PLAYER") + " of course <:smug:302980339444350976>")
+		
+	elif message.content.startswith(">8ball "):
+		randomNumber = randint(0, 19)
+		await client.send_message(message.channel, eightBall[randomNumber])
 
 def getShadowlogMessage(data, target=""):
 	if target != "":
