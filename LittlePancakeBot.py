c750922413253c8c6698ba51d0240ea660582685
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2018-09-08 22:12:52 -0700

    Added a reminder commander

--- LittlePancakeBot.py
+++ LittlePancakeBot.py
@@ -23,7 +23,22 @@ eightBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes defi
 	     "That is up for you to decide", "I don't think you should know", "Don't count on it", "My reply is no", "My sources say no", 
 	     "Unlikely", "Very doubtful", "No", "Signs point to no", "Not a chance"]
 
+reminders = []
+async def reminderHandler():
+	while True:
+		await asyncio.sleep(50)
+		print('checking reminders...')
+		for r in range(len(reminders) - 1, -1, -1):
+			print(reminders[r][0] + "|" + time.strftime("%d-%H:%M", time.gmtime()))
+			if reminders[r][0] == time.strftime("%d-%H:%M", time.gmtime()):
+				print("Reminder at " + reminders[r][0] + " excecuted.")
+				print(" Message: " + reminders[r][1])
+				await client.send_message(client.get_channel("143879568317612032"), "@everyone Reminder! " + reminders[r][1])
+				del reminders[r]
+
 client = discord.Client()
+
+client.loop.create_task(reminderHandler())
 
 @client.event
 async def on_ready():
@@ -47,21 +62,36 @@ async def on_message(message):
 		await sendEmbed(message.channel, "Good day.")
 
 	elif message.content.startswith(">help"):
-		await sendEmbed(message.channel, mainTitle="List of Commands:", desc="\n>roll (Rolls a random number between 0 and 100" + 
-			"\n>roll x (Rolls a random number between 0 and x)" +
-			"\n>8ball (Will answer a yes or no question with blistering accuracy)" +
-			"\n\n>left or right (Greatly improves your T2 skill, screenshot optional)" + 
-			"\n>which class (If you need help deciding which class to play on T2)" +
-			"\n\n>shadowlog (grabs relevant data from shadowlog, can specify class and/or format)" +
-			"\n    eg. >shadowlog forest (Grabs Forest's stats in rotation)" +
-			"\n        >shadowlog sword unlimited (Grabs Sword's stats in unlimited)" + 
-			"\n        >shadowlog unlimited (Grabs all relevant stats in unlimited)" +
-			"\n\n>anime (Searches MAL for specified anime)" + 
-			"\n>anime tv (For only TV search)" +
-			"\n>anime movie (For only movie search)" +
-			"\n>anime random (Grabs a random anime out of the best 1500 on MAL)" +
-			"\n>manga (Searches MAL for specified manga)" +
-			"\n>manga random (Grabs a random manga out of the best 1000 on MAL)", footer=True)
+		if "remind" in message.content:
+			await sendEmbed(message.channel, mainTitle="Help for >remind command:", desc="\n>remind dd-hh:mm message" +
+				"\n        ***IMPORTANT*** ALL DATES/TIMES ARE IN GMT (+0000) ***IMPORTANT***" +
+				"\n    Replace dd with the date (1, 31) for the day of the reminder. Will remind" +
+				"\non the next occurence of that day. For single digit days (1-9) add a 0 at the start." +
+				"\n    Replace hh with the hour of when you want the reminder. 24 hour time is used to" +
+				"\ndifferentiate pm and am. For single digit hours (1-9) add a 0 at the start."
+				"\n    Replace mm with the minute of when you want the reminder." +
+				"\nFor single digit months (1-9) add a 0 at the start." +
+				"\n    Notes: Reminders will not happen the second it turns to the next minute." +
+				"\nExpect there to be a 1 in 1000 chance that Pancake Bot will forget a reminder." +
+				"\nEverytime the bot recieves an update it WILL forget all current reminders.")
+		else:
+			await sendEmbed(message.channel, mainTitle="List of Commands:", desc="\n>roll (Rolls a random number between 0 and 100" + 
+				"\n>roll x (Rolls a random number between 0 and x)" +
+				"\n>8ball (Will answer a yes or no question with blistering accuracy)" +
+				"\n\n>left or right (Greatly improves your T2 skill, screenshot optional)" + 
+				"\n>which class (If you need help deciding which class to play on T2)" +
+				"\n\n>shadowlog (grabs relevant data from shadowlog, can specify class and/or format)" +
+				"\n    eg. >shadowlog forest (Grabs Forest's stats in rotation)" +
+				"\n        >shadowlog sword unlimited (Grabs Sword's stats in unlimited)" + 
+				"\n        >shadowlog unlimited (Grabs all relevant stats in unlimited)" +
+				"\n\n>remind dd-hh:mm message" +
+				"\n    Please call \">help remind\" for more information"
+				"\n\n>anime (Searches MAL for specified anime)" + 
+				"\n>anime tv (For only TV search)" +
+				"\n>anime movie (For only movie search)" +
+				"\n>anime random (Grabs a random anime out of the best 1500 on MAL)" +
+				"\n>manga (Searches MAL for specified manga)" +
+				"\n>manga random (Grabs a random manga out of the best 1000 on MAL)", footer=True)
 
 	elif message.content.startswith(">roll"):
 		try:
@@ -151,6 +181,40 @@ async def on_message(message):
 		
 	elif message.content.startswith(">8ball "):
 		await sendEmbed(message.channel, desc=choice(eightBall))
+
+	elif message.content.startswith(">remind "):
+		print('something')
+		try:
+			print('trying')
+			if isValidTime(message.content[8:]):
+				print('its valid!')
+				reminders.append([message.content[8:16], message.content[16:]])
+				sendEmbed(message.channel, desc="Reminder set for day " + 
+					message.content[8:10] + " at " + message.content[11:16])
+				print('reminder made!')
+			else:
+				print('not valid!')
+				raise
+		except:
+			print('exception risen!')
+			sendEmbed(message.channel, desc="Sorry! Looks like something went wrong while trying to set up the reminder.")
+
+def isValidTime(msg):
+	try: 
+		day = int(msg[:2])
+		if day < 0 or day > 31:
+			raise
+		if msg[2] != "-" or msg[5] != ":":
+			raise
+		hr = int(msg[3:5])
+		mn = int(msg[6:8])
+		if hr < 0 or hr > 24:
+			raise
+		if mn < 0 or mn > 60:
+			raise
+		return True
+	except:
+		return False
 
 def getSoup(link):
 	uClient = uRequest.urlopen(link)
